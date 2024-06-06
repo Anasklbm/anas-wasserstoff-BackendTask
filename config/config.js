bdd38a1f0c3cf9a60aaa13e667d5b8bd20e4fb5e
Anas Y <anasriyasniyas@gmail.com>
2024-06-06 23:29:36 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apiEndpoints: [
-        'http://localhost:3000/fast', // GET endpoint
-        'http://localhost:3000/slow', // GET endpoint
-        'http://localhost:3000/large-payload' // POST endpoint
+        'http://localhost:80/fast', // GET endpoint
+        'http://localhost:80/slow', // GET endpoint
+        'http://localhost:80/large-payload' // POST endpoint
     ]
 };
